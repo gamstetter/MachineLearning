1f25a07e75d22f70e1fceb693e226bfbe949ec54
Richard Hammond <rdhammond15@gmail.com>
2020-12-04 15:15:48 -0500

    Clean up and make more object oriented

--- Assignment5.py
+++ Assignment5.py
@@ -1,4 +1,11 @@
+#!/usr/bin/env python2
+"""
+@file Neural network comparison on sigmoid and hyperbolic tangent tranfer functions
+
+@authors: Grace Gamstetter, Michael Gentile, and Richard Hammond
+"""
 import pandas as pd
+import numpy as np
 from random import random
 from math import exp
 
@@ -9,26 +16,40 @@ class Data:
     def __init__(self):
         self.content = pd.read_csv("data_banknote_authentication.txt")
 
+class Neuron(object):
+    def __init__(self, n_inputs, transfer_func=None):
+        """
+        @brief Represent one neuron in a nural network
+
+        @param n_inputs: The number of inputs to this neuron
+        @param transfer_func: The function to linearlize the data
+        """
+        self.transfer = transfer_func
+
+        # each neuron will add a bias term to the number of inputs
+        self.weights = np.random.rand(n_inputs + 1, 1)
+
 class Network:
     """
     Only going to write this code for the banknote code.
     """
-    def __init__(self, data, num_hidden, is_sigmoid_squash):
+    def __init__(self, data, num_hidden, transfer_func):
         """
-        Instantiate the data that we need for future operations.
+        @brief Represent a neural netowrk as a colletion of rows of neurons
+        
+        @param data: The training data
+        @param num_hidden: The number of neurons in the hidden layer
+        @param transfer_func: The transfer function that the hidden layer neurons should use
         """
         self.num_hidden = num_hidden
-        self.is_sigmoid_squash = is_sigmoid_squash
         self.data = data.content.to_numpy()
+        num_inputs = len(self.data[0])
         
-        self.output_amount = len(set([row[-1] for row in self.data]))
-        neural_network = []
-        hidden_layer = [{'weights':[random() for i in range(len(self.data[0]) + 1)]} for i in range(self.num_hidden)]
-        neural_network.append(hidden_layer)
-        output_layer = [{'weights':[random() for i in range(len(self.data[0]) + 1)]} for i in range(self.output_amount)]
-        neural_network.append(output_layer)
+        self.num_classes = len(set([row[-1] for row in self.data]))
+        hidden_layer = [Neuron(num_inputs, transfer_func) for _ in range(self.num_hidden)]
+        output_layer = [Neuron(len(hidden_layer)) for _ in range(self.num_classes)]
 
-        self.neural_network = neural_network
+        self.layers = [hidden_layer, output_layer]
     
     def train_network(self, learning_rate, len_epochs):
         """
@@ -65,5 +86,6 @@ class Network:
 if __name__ == "__main__":
     data = Data()
     # TODO data needs split before here.
+    np.random.seed(1)
     network = Network(data, 3, False)
     network.train_network(0.5, 100)
